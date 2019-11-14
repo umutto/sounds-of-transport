2eaac055516b91886f033c8a53144feef14f0963
Umut Karakulak <umutkarakulak@hotmail.com>
2019-11-14 10:40:08 +0900

    git relative pathing issue

--- public/javascript/sound_controller.js
+++ public/javascript/sound_controller.js
@@ -1,15 +1,15 @@
 const audio_lib = {
-  hi_hat_1: { src: "/public/audio/hi_hat_1.mp3" },
-  hi_hat_2: { src: "/public/audio/hi_hat_2.mp3" },
-  kick_1: { src: "/public/audio/kick_1.wav" },
-  snare_1: { src: "/public/audio/snare_1.mp3" },
-  funk_1: { src: "/public/audio/funk_1.wav" },
-  funk_2: { src: "/public/audio/funk_2.wav" },
-  groovy_1: { src: "/public/audio/groovy_1.wav" },
-  jazz_1: { src: "/public/audio/jazz_1.wav" },
-  jazz_2: { src: "/public/audio/jazz_2.wav" },
-  metal_1: { src: "/public/audio/metal_1.wav" },
-  metal_2: { src: "/public/audio/metal_2.wav" }
+  hi_hat_1: { src: "public/audio/hi_hat_1.mp3" },
+  hi_hat_2: { src: "public/audio/hi_hat_2.mp3" },
+  kick_1: { src: "public/audio/kick_1.wav" },
+  snare_1: { src: "public/audio/snare_1.mp3" },
+  funk_1: { src: "public/audio/funk_1.wav" },
+  funk_2: { src: "public/audio/funk_2.wav" },
+  groovy_1: { src: "public/audio/groovy_1.wav" },
+  jazz_1: { src: "public/audio/jazz_1.wav" },
+  jazz_2: { src: "public/audio/jazz_2.wav" },
+  metal_1: { src: "public/audio/metal_1.wav" },
+  metal_2: { src: "public/audio/metal_2.wav" }
 };
 
 const inject_dom = (id, audio, loop = false, volume = 1, autoplay = true) => {
